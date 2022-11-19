7f6582ae23c3344b48f052366e7acb49110efd2e
Abdo <Abdelrahman.Hegazy@tmentors.com>
2022-11-19 14:54:29 +0200

    Initial commit

---
+++ StudentAffairsSystem/StudentAffairsSystem/StudentAffairsSystem/DesignTimeDbContextFactory.cs
@@ -0,0 +1,39 @@
+// =============================
+// Email: info@ebenmonney.com
+// www.ebenmonney.com/templates
+// =============================
+
+using AutoMapper;
+
+using DAL;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StudentAffairsSystem
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            builder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly("StudentAffairsSystem"));
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
